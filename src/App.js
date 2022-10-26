05fe8ceb8c61b3238abf1d7c993af6a50457241a
Duy Pham <ericpham19@outlook.com>
2022-10-26 13:01:23 +1100

    done with felix and francis

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
-
+import {connect} from "react-redux"
+import { fetchCats } from "./actions/catActions";
+import CatList from "./CatList"
 class App extends Component {   
-  
+  componentDidMount() {
+    this.props.fetchCats();
+  }
   render() {
+    console.log(this.props.catPics);
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        <CatList cats= {this.props.catPics}></CatList>
       </div>
     );
   }
 }
 
-export default App
+const mapStateToProps= (state) => {
+  return {
+    catPics: state.cats,
+    loading: state.loading,
+  };
+};
+const mapDispatchToProps= (dispatch) => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App);
 
--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,15 @@
 // write your CatList component here
+import React from 'react'
+import catsReducer from './reducers/catsReducer';
+ class CatList extends React.Component {
+     render(){
+        const Tom = this.props.cats.map(cat => { return <img src={cat.url} key={cat.id}></img>})
+        return <div>{Tom}</div>
+    
+     }
+     
+     
+    
+
+ }
+ export default CatList;
